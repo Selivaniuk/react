188846f73e528d8f2f2bb7f539e0aa421754e632
n-polygalov <53181166+n-polygalov@users.noreply.github.com>
2020-07-22 18:49:18 +0500

    Update cards.js

--- html_js_css/static/js/cards.js
+++ html_js_css/static/js/cards.js
@@ -1,20 +1,65 @@
-function fetchInstances() {
-    return fetch('http://tech.splinex-team.com:4999/api/cards')
-        .then(r => r.json())
-        .catch((error) => console.log(error));
-  }
-  async function createInstances() {
-    const r = await fetchInstances();
-  
-    r.services.forEach(function(e) {
-      addInstance(e);
+const url = 'http://tech.splinex-team.com:4999/api/cards';
+const d = document
+const locs = ['1018', '1014', 'cloud', 'svetly', 'pervouralsk'];
+const holder = d.getElementById('main');
+
+function fetchCards(){
+
+    return fetch(url)
+    .then(r=>{
+        return r.json();
     })
-  }
-  
-  function init() {
-    createInstances();
+    .catch(e=>{
+        console.log("error ", e);
+        return[];
+    });
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  init();
-})
+async function fetchReload(){
+    holder.cards = await fetchCards();
+    holder.cards = holder.cards.sort((a) => isErr(a.lastUpdate));
+    setTimeout(fetchReload, 1000);
+}
+    
+function isErr(lastUpdate){
+    return lastUpdate * 1000 < new Date().getTime() - 1000 * 60 * 15;;
+}
+
+ function drawCard(card, i) {
+         return `    
+         <div class="card ${isErr(card.lastUpdate) ? "error":""}">
+            <p class="num">#${i}</p>
+            <p class="ip">${card.ip}</p>           
+            <p class="metric">CORES: <span class="value">${card.cores}</span></p>
+            <p class="metric">CPU available: ${Math.round(card.cpu)} %</p>
+            <p class="metric">MEM available: ${Math.round(Math.round(card.mem)/1024/1024/1024)} GB</p>
+        </div>
+     `;
+}
+function drawCards(cards) {
+    let html1 = '';
+    for (const l of locs) {
+        const locCards = cards.filter(c=>c.location === l).sort((a, b)=>isErr(b.lastUpdate) - isErr(a.lastUpdate));
+        let locHtml = `<p class="loc-title">${l}</p>`;
+        locCards.forEach((c, i)=>{
+            locHtml+=`
+            ${drawCard(c, i+1)}
+            `;
+        });
+        html1+=locHtml;
+    }
+    holder.innerHTML = html1;
+}
+
+function drawReload() {
+    const cards = holder.cards;
+    if (cards !== undefined) {
+        drawCards(cards);
+    }
+    setTimeout(drawReload, 1000);
+}
+
+$(d).ready(function(){
+    fetchReload();
+    drawReload();
+});

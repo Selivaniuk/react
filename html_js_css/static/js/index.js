3704cf8138b2568ff817af82f1e20267105f5b33
human <human@humans-Mac-mini.local>
2020-07-22 18:03:28 +0500

    aaaaa

--- html_js_css/static/js/index.js
+++ html_js_css/static/js/index.js
@@ -6,116 +6,134 @@ function fetchInstances() {
 }
 
 async function createInstances() {
-  console.log("23");
   
   const r = await fetchInstances();
   const q = r.dt_update;
-
+  q.classList.add("qq");
+  //console.log(q);
+  
   const parent = document.getElementById('table');
-  
-  
+  const par = document.getElementsByClassName('qq');
 
   parent.innerHTML = "";
+  par.innerHTML = "";
 
-  document.getElementById("dt").innerHTML = q;
-  console.log();
+  let th1 = document.createElement('th');
+  th1.innerHTML="ID";
+  table.append(th1);
+  th1.classList.add("n");
+  let th2 = document.createElement('th');
+  th2.innerHTML="Service";
+  table.append(th2);
+  let th3 = document.createElement('th');
+  th3.innerHTML="Url";
+  table.append(th3);
+  let th4 = document.createElement('th');
+  th4.innerHTML="Deploy";
+  table.append(th4);
+  let th5 = document.createElement('th');
+  th5.innerHTML="Update";
+  table.append(th5);
+  let th6 = document.createElement('th');
+  th6.innerHTML="Status";
+  table.append(th6);
+
+
   
-
+  document.getElementById("dt").innerHTML = q;
   r.services.forEach(function(e) {
     addInstance(e);
   })
 
-  //setInterval(createInstances, 10000);
+  setInterval(createInstances, 1000 );
+  sortTable();
+  
+  
 }
 
 function addInstance(instanceData) {
     const parent = document.getElementById('table');
     const row = document.createElement('tr');
-    console.log(instanceData);
 
 
     row.className = 'table_row';
     //console.log(instanceData.status);
-    let num = '';
     let color = '';
     let array = [];
+    instanceData.key = '';
 
-    Object.values(instanceData).forEach(function (value) {
-        console.log(value);
-        Object.values(instanceData.status).forEach(function (value1) {
-            if (value1.includes('[FAIL]')) {
-                array.push('1 ' + value);
-                //console.log(value1);
-            }
-        });
-        Object.values(instanceData.status).forEach(function (value1) {
-            if (value1.includes('[WARNING]')) {
-                array.push('2 ' + value);
-            }
-        });
-        Object.values(instanceData.status).forEach(function (value1) {
-            if (value1.includes('[OK]')) {
-                array.push('3 ' + value);
-            }
-        });
+    
+    Object.values(instanceData.status).forEach(function(value) {
+      if (value.includes('[OK]')) {
+        instanceData.key= '3'
+        color = 'green';
+
+
+      };
     });
-    array.sort();
+    Object.values(instanceData.status).forEach(function(value) {
+      if (value.includes('[WARNING]')) {
+        instanceData.key= '2'
+        color = 'yellow';
+      };
+    });
+    Object.values(instanceData.status).forEach(function(value) {
+      if (value.includes('[FAIL]')) {
+        instanceData.key= '1'
+        color = 'red';
+      };
+    });
+    row.classList.add(color);
+    // if(instanceData.key){
 
-    console.log(array);
-
-    // Object.values(instanceData.status).forEach(function(value) {
-    //   if (value.includes('[OK]')) {
-    //     color = 'green';
-    //     num = '1 ';
-
-
-    //   };
-    // });
-    // Object.values(instanceData.status).forEach(function(value) {
-    //   if (value.includes('[WARNING]')) {
-    //     color = 'yellow';
-    //   };
-    // });
-    // Object.values(instanceData.status).forEach(function(value) {
-    //   if (value.includes('[FAIL]')) {
-    //     color = 'red';
-    //     num = '3 ';
-
-
-    //   };
-
-
-    // });
-    // row.classList.add(color);
-
-    // let instanceStatus = "";
-    // for (const [key, value] of Object.entries(instanceData.status)) {
-    //   instanceStatus += `<p>${key}: ${value}</p>`;
     // }
+    let instanceStatus = "";
+    for (const [key, value] of Object.entries(instanceData.status)) {
+      instanceStatus += `<p>${key}: ${value}</p>`;
+    }
 
-//   row.innerHTML = `
-//     <td>${instanceData.name}</td>
-//     <td><a href="${instanceData.url}">URL</a></td>
-//     <td>${instanceData.deployed}</td>
-//     <td>${instanceData.delay_between_tests_sec}</td>
-//     <td >${instanceStatus}</td>
-//   `;
-//   parent.append(row);
-// }
+
+
+  row.innerHTML = `
+    <td class="n" >${instanceData.key}</td>
+    <td>${instanceData.name}</td>
+    <td><a href="${instanceData.url}">URL</a></td>
+    <td>${instanceData.deployed}</td>
+    <td>${instanceData.delay_between_tests_sec}</td>
+    <td >${instanceStatus}</td>
+    
+  `;
+  parent.append(row);
+
 }
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.getElementById("table");
+  switching = true;
+  while (switching) {
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    for (i = 0; i < (rows.length - 1); i++) {
+      shouldSwitch = false;
+      x = rows[i].getElementsByTagName("TD")[0];
+      y = rows[i + 1].getElementsByTagName("TD")[0];
+      if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
+
 
 function init() {
     createInstances();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('inits');
   init();
-})
-
-
-//a
-// fetchInstances
-// createNewData
-// sortByStatus
-// drawInstances
+});

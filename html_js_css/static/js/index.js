4814a78c84bdd48c64956e02632bf355c12b8b43
Your Name <you@example.com>
2020-07-22 10:21:58 +0500

    z

--- html_js_css/static/js/index.js
+++ html_js_css/static/js/index.js
@@ -22,7 +22,12 @@ function addInstance(instanceData) {
   
 
   row.className = 'table_row';
-  //console.log(instanceData.status);
+  console.log(instanceData);
+  let arr = [];
+
+
+
+
 
   let color = '';
   Object.values(instanceData.status).forEach(function(value) {
@@ -38,6 +43,7 @@ function addInstance(instanceData) {
   Object.values(instanceData.status).forEach(function(value) {
     if (value.includes('[FAIL]')) {
       color = 'red';
+      ID =('red');
     };
   });
   row.classList.add(color);
@@ -63,39 +69,27 @@ function init() {
 document.addEventListener('DOMContentLoaded', () => {
   init();
 })
-setInterval(createInstancesc, 1000);
+setInterval(createInstances, 1000);
 
 function sortTable() {
   var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("table");
+  table = document.getElementById("myTable");
   switching = true;
-  /* Make a loop that will continue until
-  no switching has been done: */
   while (switching) {
-    // Start by saying: no switching is done:
     switching = false;
     rows = table.getElementsByTagName("TR");
-    /* Loop through all table rows (except the
-    first, which contains table headers): */
     for (i = 1; i < (rows.length - 1); i++) {
-      // Start by saying there should be no switching:
       shouldSwitch = false;
-      /* Get the two elements you want to compare,
-      one from current row and one from the next: */
       x = rows[i].getElementsByTagName("TD")[0];
       y = rows[i + 1].getElementsByTagName("TD")[0];
-      // Check if the two rows should switch place:
       if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-        // If so, mark as a switch and break the loop:
         shouldSwitch = true;
         break;
-      }
     }
     if (shouldSwitch) {
-      /* If a switch has been marked, make the switch
-      and mark that a switch has been done: */
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
     }
+  }
   }
 }

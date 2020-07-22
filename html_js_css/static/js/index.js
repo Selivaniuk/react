b4caf29b2caa8fd707079b464178e2011c890913
Your Name <vasily.komarov1997@gmail.com>
2020-07-22 12:42:05 +0500

    merge

--- html_js_css/static/js/index.js
+++ html_js_css/static/js/index.js
@@ -28,75 +28,71 @@ async function createInstances() {
   //setInterval(createInstances, 10000);
 }
 
-
-
 function addInstance(instanceData) {
-  const parent = document.getElementById('table');
-  const row = document.createElement('tr');
-  console.log(instanceData);
-  
-  
+    const parent = document.getElementById('table');
+    const row = document.createElement('tr');
+    console.log(instanceData);
 
-  row.className = 'table_row';
-  //console.log(instanceData.status);
-  let num = '';
-  let color = '';
-  let array =[];
-  
-  Object.values(instanceData).forEach(function(value) {
-    console.log(value);
-     Object.values(instanceData.status).forEach(function(value1) {
-       if (value1.includes('[FAIL]')) {
-         array.push('1 ' + value);      
-         //console.log(value1); 
-       }
-      });
-      Object.values(instanceData.status).forEach(function(value1) {
-        if (value1.includes('[WARNING]')) {
-          array.push('2 ' + value);       
-        }
-       });
-       Object.values(instanceData.status).forEach(function(value1) {
-        if (value1.includes('[OK]')) {
-          array.push('3 ' + value);       
-        }
-       });
-});
-array.sort();
 
-console.log(array);
+    row.className = 'table_row';
+    //console.log(instanceData.status);
+    let num = '';
+    let color = '';
+    let array = [];
 
-  // Object.values(instanceData.status).forEach(function(value) {
-  //   if (value.includes('[OK]')) {
-  //     color = 'green';
-  //     num = '1 ';
+    Object.values(instanceData).forEach(function (value) {
+        console.log(value);
+        Object.values(instanceData.status).forEach(function (value1) {
+            if (value1.includes('[FAIL]')) {
+                array.push('1 ' + value);
+                //console.log(value1);
+            }
+        });
+        Object.values(instanceData.status).forEach(function (value1) {
+            if (value1.includes('[WARNING]')) {
+                array.push('2 ' + value);
+            }
+        });
+        Object.values(instanceData.status).forEach(function (value1) {
+            if (value1.includes('[OK]')) {
+                array.push('3 ' + value);
+            }
+        });
+    });
+    array.sort();
 
-      
+    console.log(array);
 
-  //   };
-  // });
-  // Object.values(instanceData.status).forEach(function(value) {
-  //   if (value.includes('[WARNING]')) {
-  //     color = 'yellow';
-  //   };
-  // });
-  // Object.values(instanceData.status).forEach(function(value) {
-  //   if (value.includes('[FAIL]')) {
-  //     color = 'red';
-  //     num = '3 ';
-      
-      
-  //   };
-   
-    
-  // });
-  // row.classList.add(color);
-  
-  // let instanceStatus = "";
-  // for (const [key, value] of Object.entries(instanceData.status)) {
-  //   instanceStatus += `<p>${key}: ${value}</p>`;
-  // }
-  
+    // Object.values(instanceData.status).forEach(function(value) {
+    //   if (value.includes('[OK]')) {
+    //     color = 'green';
+    //     num = '1 ';
+
+
+    //   };
+    // });
+    // Object.values(instanceData.status).forEach(function(value) {
+    //   if (value.includes('[WARNING]')) {
+    //     color = 'yellow';
+    //   };
+    // });
+    // Object.values(instanceData.status).forEach(function(value) {
+    //   if (value.includes('[FAIL]')) {
+    //     color = 'red';
+    //     num = '3 ';
+
+
+    //   };
+
+
+    // });
+    // row.classList.add(color);
+
+    // let instanceStatus = "";
+    // for (const [key, value] of Object.entries(instanceData.status)) {
+    //   instanceStatus += `<p>${key}: ${value}</p>`;
+    // }
+
 //   row.innerHTML = `
 //     <td>${instanceData.name}</td>
 //     <td><a href="${instanceData.url}">URL</a></td>
@@ -106,23 +102,13 @@ console.log(array);
 //   `;
 //   parent.append(row);
 // }
+}
+
 function init() {
     createInstances();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('inits');
-  
   init();
 })
-}
-
-//a
-// fetchInstances
-// createNewData
-// sortByStatus
-<<<<<<< HEAD
-// drawInstances
-=======
-// drawInstances
->>>>>>> fbaef75bd2127d48a83c658d86ee3737e116eebb

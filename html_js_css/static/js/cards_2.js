b642a6a728ad5f4ef23935397df0fec2f151ffae
Your Name <vasily.komarov1997@gmail.com>
2020-07-24 12:41:33 +0500

    fix card numbers

--- html_js_css/static/js/cards_2.js
+++ html_js_css/static/js/cards_2.js
@@ -2,70 +2,66 @@ function fetchInstances() {
     return fetch('http://tech.splinex-team.com:4999/api/cards')
         .then(r => r.json())
         .catch((error) => console.log(error));
-  }
-  async function createInstances() {
+}
+async function createInstances() {
     const r = await fetchInstances();
 
     //console.log(r);
     const parent22 = document.getElementById('main');
     parent22.innerHTML = "";
-    let p1 = document.createElement('p');
+    const p1 = document.createElement('p');
     p1.innerHTML="1018";
-    p1.className="pp"
+    p1.className="pp";
 
-    let d_1018 = document.createElement('div');
+    const d_1018 = document.createElement('div');
     d_1018.id = 'd_1018';
     d_1018.classList.add('d_1018');
 
-    let p2 = document.createElement('p');
+    const p2 = document.createElement('p');
     p2.innerHTML="1014";
-    p2.className="pp"
+    p2.className="pp";
 
-    let d_1014 = document.createElement('div');
+    const d_1014 = document.createElement('div');
     d_1014.id = 'd_1014';
     d_1014.classList.add('d_1014');
 
-    let p3 = document.createElement('p');
+    const p3 = document.createElement('p');
     p3.innerHTML="CLOUD";
-    p3.className="pp"
+    p3.className="pp";
 
-    let div_cloud = document.createElement('div');
+    const div_cloud = document.createElement('div');
     div_cloud.id = 'cloud';
     div_cloud.classList.add('cloud');
 
-    let p4 = document.createElement('p');
+    const p4 = document.createElement('p');
     p4.innerHTML="SVETLY";
-    p4.className="pp"
+    p4.className="pp";
 
 
-    let svetly = document.createElement('div');
+    const svetly = document.createElement('div');
     svetly.id = 'svetly';
     svetly.classList.add('svetly');
 
-    let p5 = document.createElement('p');
+    const p5 = document.createElement('p');
     p5.innerHTML="PERVOURALSK";
-    p5.className="pp"
+    p5.className="pp";
 
-    let pervouralsk = document.createElement('div');
+    const pervouralsk = document.createElement('div');
     pervouralsk.id = 'pervouralsk';
     pervouralsk.classList.add('pervouralsk');
 
 
-    var currentDiv = document.getElementById("main"); 
-    currentDiv.append(p1,d_1018,p2,d_1014,p3,div_cloud,p4,svetly,p5,pervouralsk);
-    
-    r.forEach(function(e) {
-      addInstance(e);
+    var currentDiv = document.getElementById("main");
+    currentDiv.append(p1, d_1018, p2, d_1014, p3, div_cloud, p4, svetly, p5, pervouralsk);
+
+    r.forEach(function(e, i) {
+        addInstance(e, i);
     });
 
-      //createInstances();
-         setTimeout(createInstances, 1000 ); 
-  
-  }
-  function addInstance(instanceData) {
-
-
-
+    //createInstances();
+    setTimeout(createInstances, 1000 );
+}
+function addInstance(instanceData, i) {
     const parent1 = document.getElementById('cloud');
     const parent2 = document.getElementById('d_1018');
     const parent3 = document.getElementById('d_1014');
@@ -80,42 +76,36 @@ function fetchInstances() {
 
 
     array1.forEach(function(value) {
-        if (value>0){
+        if (value>0) {
             color = "green";
         }
-        if (value == 0){
+        if (value == 0) {
             color = "red";
         }
-        
-       
-   });
-   instanceData.em = '';
-   tex.className = color
-  instanceData.em = instanceData.mem/1024/1024/1024;
-  let a = 0;
-   array2.forEach(function(value ,i){
-    console.log(i);
-    
-   
-       if(value == 'cloud'){
+    });
+    instanceData.em = '';
+    tex.className = color;
+    instanceData.em = instanceData.mem/1024/1024/1024;
+    console.log(array2);
+    array2.forEach(function(value) {
+        console.log(i);
 
-        a++;  
 
-        tex.innerHTML = `
+        if (value == 'cloud') {
+            tex.innerHTML = `
+                <p>#${i}</p>
+                <h1>${instanceData.ip}</h1>
+                <p>${'CORES: ' + instanceData.cores}</p>
+                <p>${'CPU avilable: ' + Math.floor(instanceData.cpu) + '%'}</p>
+                <p>${'MEM avilable: ' + Math.floor(instanceData.em)}</p>
+            `;
+            parent1.append(tex);
+        }
 
-        <p>#${i}</p>
-        <h1>${instanceData.ip}</h1>
-        <p>${'CORES: ' + instanceData.cores}</p>
-        <p>${'CPU avilable: ' + Math.floor(instanceData.cpu) + '%'}</p>
-        <p>${'MEM avilable: ' + Math.floor(instanceData.em)}</p>
-        
-      `;
-        parent1.append(tex);
-       }
-       if(value == '1018'){
-        console.log(value.length);
+        if (value == '1018') {
+            console.log(value.length);
 
-        tex.innerHTML = `
+            tex.innerHTML = `
         <h1>${instanceData.ip}</h1>
         <p>${'CORES: ' + instanceData.cores}</p>
         <p>${'CPU avilable: ' + Math.floor(instanceData.cpu) + '%'}</p>
@@ -124,11 +114,10 @@ function fetchInstances() {
 
         
       `;
-        parent2.append(tex);
-       }
-       if(value == '1014'){
-
-        tex.innerHTML = `
+            parent2.append(tex);
+        }
+        if (value == '1014') {
+            tex.innerHTML = `
         <h1>${instanceData.ip}</h1>
         <p>${'CORES: ' + instanceData.cores}</p>
         <p>${'CPU avilable: ' + Math.floor(instanceData.cpu) + '%'}</p>
@@ -136,11 +125,10 @@ function fetchInstances() {
 
         
       `;
-        parent3.append(tex);
-       }
-       if(value == 'svetly'){
-
-        tex.innerHTML = `
+            parent3.append(tex);
+        }
+        if (value == 'svetly') {
+            tex.innerHTML = `
         <h1>${instanceData.ip}</h1>
         <p>${'CORES: ' + instanceData.cores}</p>
         <p>${'CPU avilable: ' + Math.floor(instanceData.cpu) + '%'}</p>
@@ -148,11 +136,10 @@ function fetchInstances() {
 
         
       `;
-        parent4.append(tex);
-       }
-       if(value == 'pervouralsk'){
-
-        tex.innerHTML = `
+            parent4.append(tex);
+        }
+        if (value == 'pervouralsk') {
+            tex.innerHTML = `
         <h1>${instanceData.ip}</h1>
         <p>${'CORES: ' + instanceData.cores}</p>
         <p>${'CPU avilable: ' + Math.floor(instanceData.cpu) + '%'}</p>
@@ -160,18 +147,15 @@ function fetchInstances() {
 
         
       `;
-        parent5.append(tex);
-       }
-
-  });
-
+            parent5.append(tex);
+        }
+    });
 }
 function init() {
-  createInstances();
+    createInstances();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-init();
+    init();
 });
-  
-  
+
